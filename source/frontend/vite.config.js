91effaf509e11704d91315efdcc7beff29030815
Stefano Vozza <svozza@amazon.com>
2023-01-13 10:44:14 +0000

    add comment about amplify to vite config file

--- source/frontend/vite.config.js
+++ source/frontend/vite.config.js
@@ -29,6 +29,7 @@ export default defineConfig({
         }
     ],
     resolve: {
+        // this is required for Amplify
         alias: {
             './runtimeConfig': './runtimeConfig.browser',
         },
